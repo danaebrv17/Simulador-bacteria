c45bbd7f0b83d10765b7b4d66205347ab6c71354
danaebrv17 <bravodanae277@gmail.com>
2025-07-09 22:55:38 -0400

    Update bacteria.py
    agrego metodo dividirse y mutar

--- proyecto/bacteria.py
+++ proyecto/bacteria.py
@@ -26,3 +26,19 @@ class Bacteria:
     def morir(self):
         self.viva = False
         self.energia = 0
+ # Metodo para dividirse en una hija si tiene energia suficiente
+    def dividirse(self, nuevo_id):
+        if self.viva and self.energia >= 20: #para dividirse tendra 20 o mas de energia
+            energia_hija = self.energia // 2
+            self.energia = energia_hija #creo onjeto bacteria nuevo
+            hija = Bacteria(nuevo_id, self.raza, energia_hija, self.resistente)
+            hija.recien_nacida = True
+            return hija
+        return None
+
+    # Metodo para mutar con cierta probabilidad
+    def mutar(self, probabilidad=0.1):
+        import random 
+        if self.viva and not self.resistente: #mutara solo si esta viva y no es resistente
+            if random.random() < probabilidad: #si el numero es menor a 0.1 se vuelve resistente
+                self.resistente = True
