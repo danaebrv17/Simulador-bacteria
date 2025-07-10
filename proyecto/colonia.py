022b42e76c9487dc58429f0b6eea51a9ddaf18d1
danaebrv17 <bravodanae277@gmail.com>
2025-07-09 23:05:50 -0400

    Update colonia.py
    creo clase colonia y administra lista de bacterias y contiene a ambiente

--- proyecto/colonia.py
+++ proyecto/colonia.py
@@ -2,3 +2,14 @@
 #bacteria como lista de objetos
 #ambiente como instancia de clase ambiente
 #metodo paso(), reporte estado(), exportar csv()
+import random
+import csv
+from bacteria import Bacteria
+
+# Clase que administra la colonia de bacterias en el ambiente
+class Colonia:
+    def __init__(self, ambiente): # colonia contiene al ambiente
+        self.ambiente = ambiente
+        self.bacterias = []
+        self.contador_id = 1
+      
