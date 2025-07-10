ed1c7d6a78b00ff1f67741b0624ff87e4cc1ba61
danaebrv17 <bravodanae277@gmail.com>
2025-07-09 22:35:49 -0400

    Update bacteria.py
    agrego constructor en bacteria y sus atributos

--- proyecto/bacteria.py
+++ proyecto/bacteria.py
@@ -1,7 +1,26 @@
-# Clase que representara una bacteria individual
-#debe tener:
 #id, raza
 # energia
 # resistente
 #estado
 #metodos alimentar(), dividirse(), mutar()y morir()
+
+# Clase que representa una bacteria individual
+class Bacteria:
+    def __init__(self, id_bacteria, raza):
+        # identificador unico de la bacteria
+        self.id = id_bacteria
+
+        # raza de la bacteria (por ejemplo: "A" o "B")
+        self.raza = raza
+
+        # energia actual de la bacteria
+        self.energia = 5
+
+        # si la bacteria es resistente a ataques o no
+        self.resistente = False
+
+        # si la bacteria esta viva o no
+        self.viva = True
+
+        self.recien_nacida = False
+        self.fue_atacada = False
